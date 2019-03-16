6bed1198bda20de0da03f5934bdea7ca9ccdb196
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-16 00:37:05 -0300

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,23 +1,8 @@
 import React, { Component } from 'react'
 import { graphql } from 'gatsby'
 import { Error404 } from 'Components/Error404'
-import ThemeContext from 'Context/ThemeContext'
 
 class PageNotFound extends Component {
-  static contextType = ThemeContext
-
-  componentDidMount () {
-    const { setNotFound } = this.context
-
-    setNotFound()
-  }
-
-  componentWillUnmount () {
-    const { setFound } = this.context
-
-    setFound()
-  }
-
   render () {
     return <Error404 data={this.props.data} />
   }
