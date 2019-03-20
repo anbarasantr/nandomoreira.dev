c5631d728f9e5c7dfbf2fce4b181788c9caa168f
Fernando Moreira <nandomoreira.me@gmail.com>
2019-03-20 04:10:25 -0300

    fix recommendations config

---
+++ data/recommendations.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    photo: 'douglas.jpg',
+    name: 'Douglas Martinello Karling',
+    name: 'Web Developer at Observatórios Sistema Fiep',
+    testimonial: 'Excelente profissional, comprometido e dedicado, gosta de compartilhar seus conhecimentos e sempre busca estar antenado com as novidades da área.',
+  },
+  {
+    photo: 'celso.jpg',
+    name: 'Celso Fabri Junior',
+    name: 'Web developer at EBANX',
+    testimonial: 'Excelente e jovem profissional com amplo conhecimento na área web. Além de ser responsável e pró-ativo, busca aperfeiçoamento e melhorias para processos de desenvolvimento mais ágeis e eficazes.',
+  },
+  {
+    photo: 'bruno.jpg',
+    name: 'Bruno Medeiros',
+    name: 'Digital Marketing | Marketing Cloud',
+    testimonial: 'O Fernando é uma pessoa que além de ser um excelente profissional alegra os dias do pessoal aqui da empresa! Um dos melhores Front-End que eu conheço.',
+  },
+]
